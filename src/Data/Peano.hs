c1495d460695e15d0bef70a620268c31cfc41e2c
godu <arthur.weber@viacesi.fr>
2019-09-29 00:34:16 +0200

    Peano

--- src/Data/Peano.hs
+++ src/Data/Peano.hs
@@ -1,8 +1,16 @@
 module Data.Peano where
 
+import Data.Ix (Ix(..))
+
 data Peano
   = Zero
   | Succ Peano
+
+instance Eq Peano where
+  (==) Zero Zero = True
+  (==) Zero _ = False
+  (==) _ Zero = False
+  (==) (Succ a) (Succ b) = a == b
 
 instance Enum Peano where
   fromEnum Zero = 0
@@ -11,12 +19,9 @@ instance Enum Peano where
   toEnum x
     | x > 0 = Succ (toEnum (x - 1))
     | otherwise = error ("toEnum " <> show x)
-
-instance Eq Peano where
-  (==) Zero Zero = True
-  (==) Zero _ = False
-  (==) _ Zero = False
-  (==) (Succ a) (Succ b) = a == b
+  succ = Succ
+  pred Zero = error "pred Zero"
+  pred (Succ a) = a
 
 instance Show Peano where
   show = show . fromEnum
@@ -29,3 +34,48 @@ instance Ord Peano where
 
 instance Read Peano where
   readsPrec d = map (\(a, b) -> (toEnum a, b)) . readsPrec d
+
+instance Num Peano where
+  a + Zero = a
+  a + Succ b = succ (a + b)
+  Zero - Zero = Zero
+  Zero - _ = error "minus < 0"
+  a - Zero = a
+  Succ a - Succ b = a - b
+  Zero * _ = Zero
+  _ * Zero = Zero
+  a * Succ Zero = a
+  a * Succ b = a + a * b
+  abs = id
+  signum Zero = Zero
+  signum _ = Succ Zero
+  fromInteger a
+    | a == 0 = Zero
+    | a > 0 = Succ (fromInteger (a - 1))
+    | a < 0 = error "fromInteger < 0"
+
+infinity :: Peano
+infinity = Succ infinity
+
+instance Bounded Peano where
+  minBound = Zero
+  maxBound = infinity
+
+instance Ix Peano where
+  range (a, b) = enumFromTo a b
+  index (a, _) b = fromEnum (b - a)
+  inRange (a, b) c = a <= c && b >= c
+
+instance Real Peano where
+  toRational = toRational . toInteger
+
+instance Integral Peano where
+  toInteger Zero = 0
+  toInteger (Succ a) = toInteger a + 1
+  quotRem Zero Zero = error "0/0"
+  quotRem a b =
+    case compare a b of
+      LT -> (Zero, a)
+      _ ->
+        let (q, r) = quotRem (a - b) b
+         in (Succ q, r)
--- test/Data/PeanoSpec.hs
+++ test/Data/PeanoSpec.hs
@@ -1,6 +1,7 @@
 module Data.PeanoSpec where
 
 import Control.Exception (evaluate)
+import Data.Ix (Ix(..), inRange, index, range)
 import Data.Peano (Peano(..))
 import Test.Hspec (Spec, errorCall, hspec, it, shouldBe, shouldThrow)
 
@@ -16,12 +17,16 @@ spec = do
   it "Enum" $ do
     toEnum 0 `shouldBe` Zero
     toEnum 1 `shouldBe` Succ Zero
+    evaluate (toEnum (-1) :: Peano) `shouldThrow` errorCall "toEnum -1"
     fromEnum Zero `shouldBe` 0
     fromEnum (Succ Zero) `shouldBe` 1
     succ Zero `shouldBe` Succ Zero
     succ (Succ Zero) `shouldBe` Succ (Succ Zero)
     pred (Succ Zero) `shouldBe` Zero
-    evaluate (pred Zero) `shouldThrow` errorCall "toEnum -1"
+    evaluate (pred Zero) `shouldThrow` errorCall "pred Zero"
+  it "Show" $ do
+    show Zero `shouldBe` "0"
+    show (Succ Zero) `shouldBe` "1"
   it "Ord" $ do
     Zero <= Zero `shouldBe` True
     (Zero <= Succ Zero) `shouldBe` True
@@ -30,5 +35,39 @@ spec = do
   it "Read" $ do
     read "0" `shouldBe` Zero
     read "1" `shouldBe` Succ Zero
+    evaluate (read "-1" :: Peano) `shouldThrow` errorCall "toEnum -1"
     evaluate (read "a" :: Peano) `shouldThrow`
       errorCall "Prelude.read: no parse"
+  it "Num" $ do
+    Succ (Succ Zero) + Succ Zero `shouldBe` Succ (Succ (Succ Zero))
+    Succ Zero + Succ (Succ Zero) `shouldBe` Succ (Succ (Succ Zero))
+    Succ (Succ Zero) - Succ Zero `shouldBe` Succ Zero
+    evaluate (Zero - Succ Zero) `shouldThrow` errorCall "minus < 0"
+    Succ Zero * Zero `shouldBe` Zero
+    Zero * Succ Zero `shouldBe` Zero
+    Succ (Succ Zero) *
+      Succ (Succ Zero) `shouldBe` Succ (Succ (Succ (Succ Zero)))
+    abs (Succ Zero) `shouldBe` Succ Zero
+    signum Zero `shouldBe` Zero
+    signum (Succ (Succ Zero)) `shouldBe` Succ Zero
+    (2 :: Peano) `shouldBe` Succ (Succ Zero)
+    (0 :: Peano) `shouldBe` Zero
+    evaluate (fromInteger (-2) :: Peano) `shouldThrow`
+      errorCall "fromInteger < 0"
+  it "Ix" $ do
+    range (Zero, Succ (Succ Zero)) `shouldBe`
+      [Zero, Succ Zero, Succ (Succ Zero)]
+    range (Zero, Zero) `shouldBe` [Zero]
+    range (Succ Zero, Zero) `shouldBe` []
+    index (Zero, Succ (Succ Zero)) (Succ Zero) `shouldBe` 1
+    inRange (Zero, Succ (Succ Zero)) (Succ Zero) `shouldBe` True
+    inRange (Zero, Succ Zero) (Succ (Succ Zero)) `shouldBe` False
+  it "Real" $ do
+    toRational Zero `shouldBe` 0
+    toRational (Succ Zero) `shouldBe` 1
+  it "Integral" $ do
+    toInteger Zero `shouldBe` 0
+    toInteger (Succ Zero) `shouldBe` 1
+    quotRem (Succ Zero) (Succ (Succ Zero)) `shouldBe` (Zero, Succ Zero)
+    quotRem (Succ (Succ Zero)) (Succ Zero) `shouldBe` (Succ (Succ Zero), Zero)
+    evaluate (quotRem Zero Zero) `shouldThrow` errorCall "0/0"
