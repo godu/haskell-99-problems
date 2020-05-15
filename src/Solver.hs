2109f7942fcf5ee318adbd8febd632faca811be4
godu <arthur.weber@viacesi.fr>
2020-05-15 19:15:16 +0200

    Sudoku

--- src/Solver.hs
+++ src/Solver.hs
@@ -126,3 +126,60 @@ linearOpt = do
   constrain $ x2 .>= 0
 
   maximize "goal" $ 5 * x1 + 6 * x2
+
+xkcd :: Goal
+xkcd = do
+  [a, b, c, d, e, f] <- sInt8s ["a", "b", "c", "d", "e", "f"]
+
+  constrain $ foldl (.&&) sTrue $ (\x -> x .>= 0) <$> [a, b, c, d, e, f]
+
+  let [a_, b_, c_, d_, e_, f_] =
+        toSDouble sRoundTowardZero <$> [a, b, c, d, e, f]
+
+  constrain
+    $   ( (a_ * 2.15)
+        + (b_ * 2.75)
+        + (c_ * 3.35)
+        + (d_ * 3.55)
+        + (e_ * 5.20)
+        + (f_ * 5.80)
+        )
+    .== 15.05
+
+type Row = [SWord8]
+type Board = [Row]
+
+sudoku :: (Int, [SWord8] -> Board) -> Goal
+sudoku (i, mkBoard) = do
+  board <- mkBoard <$> mkExistVars i
+  sudokuConstrains board
+ where
+  rows board = board
+  columns [[a1, a2, a3, a4, a5, a6, a7, a8, a9], [b1, b2, b3, b4, b5, b6, b7, b8, b9], [c1, c2, c3, c4, c5, c6, c7, c8, c9], [d1, d2, d3, d4, d5, d6, d7, d8, d9], [e1, e2, e3, e4, e5, e6, e7, e8, e9], [f1, f2, f3, f4, f5, f6, f7, f8, f9], [g1, g2, g3, g4, g5, g6, g7, g8, g9], [h1, h2, h3, h4, h5, h6, h7, h8, h9], [i1, i2, i3, i4, i5, i6, i7, i8, i9]]
+    = [ [a1, b1, c1, d1, e1, f1, g1, h1, i1]
+      , [a2, b2, c2, d2, e2, f2, g2, h2, i2]
+      , [a3, b3, c3, d3, e3, f3, g3, h3, i3]
+      , [a4, b4, c4, d4, e4, f4, g4, h4, i4]
+      , [a5, b5, c5, d5, e5, f5, g5, h5, i5]
+      , [a6, b6, c6, d6, e6, f6, g6, h6, i6]
+      , [a7, b7, c7, d7, e7, f7, g7, h7, i7]
+      , [a8, b8, c8, d8, e8, f8, g8, h8, i8]
+      , [a9, b9, c9, d9, e9, f9, g9, h9, i9]
+      ]
+  regions [[a1, a2, a3, a4, a5, a6, a7, a8, a9], [b1, b2, b3, b4, b5, b6, b7, b8, b9], [c1, c2, c3, c4, c5, c6, c7, c8, c9], [d1, d2, d3, d4, d5, d6, d7, d8, d9], [e1, e2, e3, e4, e5, e6, e7, e8, e9], [f1, f2, f3, f4, f5, f6, f7, f8, f9], [g1, g2, g3, g4, g5, g6, g7, g8, g9], [h1, h2, h3, h4, h5, h6, h7, h8, h9], [i1, i2, i3, i4, i5, i6, i7, i8, i9]]
+    = [ [a1, a2, a3, b1, b2, b3, c1, c2, c3]
+      , [a4, a5, a6, b4, b5, b6, c4, c5, c6]
+      , [a7, a8, a9, b7, b8, b9, c7, c8, c9]
+      , [d1, d2, d3, e1, e2, e3, f1, f2, f3]
+      , [d4, d5, d6, e4, e5, e6, f4, f5, f6]
+      , [d7, d8, d9, e7, e8, e9, f7, f8, f9]
+      , [g1, g2, g3, h1, h2, h3, i1, i2, i3]
+      , [g4, g5, g6, h4, h5, h6, i4, i5, i6]
+      , [g7, g8, g9, h7, h8, h9, i7, i8, i9]
+      ]
+  sudokuConstrains board =
+    constrain
+      $  foldl (.&&) sTrue
+      $  (distinct <$> ((rows board) <> (columns board) <> (regions board)))
+      <> ((.>= 1) <$> concat board)
+      <> ((.<= 9) <$> concat board)
--- test/SolverSpec.hs
+++ test/SolverSpec.hs
@@ -22,11 +22,99 @@ spec = do
         actual <- satWith z3Debug uninterpretedFunctionsAndConstants
         (show actual)
             `shouldBe` "Satisfiable. Model:\n\
-                                 \  a = 21 :: Integer\n\
-                                 \  b = 22 :: Integer\n\
-                                 \\n\
-                                 \  f :: Integer -> Integer\n\
-                                 \  f _ = 1"
+                       \  a = 21 :: Integer\n\
+                       \  b = 22 :: Integer\n\
+                       \\n\
+                       \  f :: Integer -> Integer\n\
+                       \  f _ = 1"
+
+    it "xkcd 287" $ do
+        actual <- allSat xkcd
+        (show actual)
+            `shouldBe` "Solution #1:\n\
+                       \  a = 1 :: Int8\n\
+                       \  b = 0 :: Int8\n\
+                       \  c = 0 :: Int8\n\
+                       \  d = 2 :: Int8\n\
+                       \  e = 0 :: Int8\n\
+                       \  f = 1 :: Int8\n\
+                       \Solution #2:\n\
+                       \  a = 0 :: Int8\n\
+                       \  b = 1 :: Int8\n\
+                       \  c = 0 :: Int8\n\
+                       \  d = 2 :: Int8\n\
+                       \  e = 1 :: Int8\n\
+                       \  f = 0 :: Int8\n\
+                       \Found 2 different solutions."
+
+    it "sudoku" $ do
+        let mkBoard [a3, a4, a6, a7, a8, a9, b2, b3, b7, b8, b9, c1, c4, c5, c6, c7, c9, d2, d3, d4, d6, d7, d8, e2, e3, e5, e7, e8, f2, f3, f4, f6, f7, f8, g1, g3, g4, g5, g6, g9, h1, h2, h3, h7, h8, i1, i2, i3, i4, i6, i7]
+                = [ [5, 3, a3, a4, 7, a6, a7, a8, a9]
+                  , [6, b2, b3, 1, 9, 5, b7, b8, b9]
+                  , [c1, 9, 8, c4, c5, c6, c7, 6, c9]
+                  , [8, d2, d3, d4, 6, d6, d7, d8, 3]
+                  , [4, e2, e3, 8, e5, 3, e7, e8, 1]
+                  , [7, f2, f3, f4, 2, f6, f7, f8, 6]
+                  , [g1, 6, g3, g4, g5, g6, 2, 8, g9]
+                  , [h1, h2, h3, 4, 1, 9, h7, h8, 5]
+                  , [i1, i2, i3, i4, 8, i6, i7, 7, 9]
+                  ]
+            mkBoard _ = error "puzzle0 needs exactly 81 elements!"
+        actual <- satWith z3Debug (sudoku (51, mkBoard))
+        (show actual)
+            `shouldBe` "Satisfiable. Model:\n\
+                       \  s0  = 4 :: Word8\n\
+                       \  s1  = 6 :: Word8\n\
+                       \  s2  = 8 :: Word8\n\
+                       \  s3  = 9 :: Word8\n\
+                       \  s4  = 1 :: Word8\n\
+                       \  s5  = 2 :: Word8\n\
+                       \  s6  = 7 :: Word8\n\
+                       \  s7  = 2 :: Word8\n\
+                       \  s8  = 3 :: Word8\n\
+                       \  s9  = 4 :: Word8\n\
+                       \  s10 = 8 :: Word8\n\
+                       \  s11 = 1 :: Word8\n\
+                       \  s12 = 3 :: Word8\n\
+                       \  s13 = 4 :: Word8\n\
+                       \  s14 = 2 :: Word8\n\
+                       \  s15 = 5 :: Word8\n\
+                       \  s16 = 7 :: Word8\n\
+                       \  s17 = 5 :: Word8\n\
+                       \  s18 = 9 :: Word8\n\
+                       \  s19 = 7 :: Word8\n\
+                       \  s20 = 1 :: Word8\n\
+                       \  s21 = 4 :: Word8\n\
+                       \  s22 = 2 :: Word8\n\
+                       \  s23 = 2 :: Word8\n\
+                       \  s24 = 6 :: Word8\n\
+                       \  s25 = 5 :: Word8\n\
+                       \  s26 = 7 :: Word8\n\
+                       \  s27 = 9 :: Word8\n\
+                       \  s28 = 1 :: Word8\n\
+                       \  s29 = 3 :: Word8\n\
+                       \  s30 = 9 :: Word8\n\
+                       \  s31 = 4 :: Word8\n\
+                       \  s32 = 8 :: Word8\n\
+                       \  s33 = 5 :: Word8\n\
+                       \  s34 = 9 :: Word8\n\
+                       \  s35 = 1 :: Word8\n\
+                       \  s36 = 5 :: Word8\n\
+                       \  s37 = 3 :: Word8\n\
+                       \  s38 = 7 :: Word8\n\
+                       \  s39 = 4 :: Word8\n\
+                       \  s40 = 2 :: Word8\n\
+                       \  s41 = 8 :: Word8\n\
+                       \  s42 = 7 :: Word8\n\
+                       \  s43 = 6 :: Word8\n\
+                       \  s44 = 3 :: Word8\n\
+                       \  s45 = 3 :: Word8\n\
+                       \  s46 = 4 :: Word8\n\
+                       \  s47 = 5 :: Word8\n\
+                       \  s48 = 2 :: Word8\n\
+                       \  s49 = 6 :: Word8\n\
+                       \  s50 = 1 :: Word8"
+
 
     it "Production" $ do
         actual <- optimize Lexicographic production
