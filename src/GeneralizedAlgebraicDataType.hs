e24fd6b2c8a1b9931541fe3db82da0a4f34c9833
godu <arthur.weber@viacesi.fr>
2020-06-01 17:31:42 +0200

    GADT

--- src/GeneralizedAlgebraicDataType.hs
+++ src/GeneralizedAlgebraicDataType.hs
@@ -7,10 +7,14 @@ data Expression a where
   B ::Bool -> Expression Bool
   Add ::Expression Int -> Expression Int -> Expression Int
   Mul ::Expression Int -> Expression Int -> Expression Int
-  Eq ::Eq a => Expression a -> Expression a -> Expression Bool
+  Eq ::(Eq a, Show a) => Expression a -> Expression a -> Expression Bool
 
 instance (Show a) => Show (Expression a) where
-  show (I n) = show n <> " : Int"
+  show (I a    ) = show a <> " : Int"
+  show (B a    ) = show a <> " : Bool"
+  show (Add a b) = "(" <> show a <> ") + (" <> show b <> ") : Int"
+  show (Mul a b) = "(" <> show a <> ") * (" <> show b <> ") : Int"
+  show (Eq  a b) = "(" <> show a <> ") == (" <> show b <> ") : Bool"
 
 evaluate :: Expression a -> a
 evaluate (I a    ) = a
--- test/GeneralizedAlgebraicDataTypeSpec.hs
+++ test/GeneralizedAlgebraicDataTypeSpec.hs
@@ -1,17 +1,21 @@
 module GeneralizedAlgebraicDataTypeSpec where
 
-import           Prelude
-import           Test.Hspec
 import           GeneralizedAlgebraicDataType
+import           Test.Hspec
 
 spec :: Spec
 spec = do
-  describe "GeneralizedAlgebraicDatatype" $ do
-    it "show" $ do
-      show (I 1) `shouldBe` "1 : Int"
-    it "evaluate" $ do
-      evaluate (I 1) `shouldBe` 1
-      evaluate (B False) `shouldBe` False
-      evaluate (Add (I 1) (I 2)) `shouldBe` 3
-      evaluate (Mul (I 2) (I 3)) `shouldBe` 6
-      evaluate (Eq (I 1) (I 2)) `shouldBe` False
+  describe "show" $ do
+    it "I" $ show (I 1) `shouldBe` "1 : Int"
+    it "B" $ show (B True) `shouldBe` "True : Bool"
+    it "Add" $ show (Add (I 1) (I 2)) `shouldBe` "(1 : Int) + (2 : Int) : Int"
+    it "Mul" $ show (Mul (I 2) (I 3)) `shouldBe` "(2 : Int) * (3 : Int) : Int"
+    it "Eq"
+      $ show (Eq (Add (I 2) (I 4)) (Mul (I 2) (I 3)))
+      `shouldBe` "((2 : Int) + (4 : Int) : Int) == ((2 : Int) * (3 : Int) : Int) : Bool"
+  describe "evaluate" $ do
+    it "I" $ evaluate (I 1) `shouldBe` 1
+    it "B" $ evaluate (B True) `shouldBe` True
+    it "Add" $ evaluate (Add (I 1) (I 2)) `shouldBe` 3
+    it "Mul" $ evaluate (Mul (I 2) (I 3)) `shouldBe` 6
+    it "Eq" $ evaluate (Eq (Add (I 2) (I 4)) (Mul (I 2) (I 3))) `shouldBe` True
